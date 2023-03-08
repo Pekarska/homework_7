5343c82b22a150981f0488afa37daa6d547d11be
Anna Pekarska <anya.pekarskaya1997@gmail.com>
2023-03-08 21:58:56 +0200

    add unziping files, add removing empty folders, add printing outputs

--- my_project.py
+++ my_project.py
@@ -1,40 +1,12 @@
+import os
 from pathlib import Path
 import re
 import shutil
 import sys
 
 
-
-folder_for_parse = sys.argv[1]
-
- 
-
-
-images_folder = folder_for_parse + '/images'
-im = Path(images_folder)
-im.mkdir(exist_ok=True)
-
-documents_folder = folder_for_parse + '/documents'
-doc = Path(documents_folder)
-doc.mkdir(exist_ok=True)
-
-audio_folder = folder_for_parse + '/audio'
-aud = Path(audio_folder)
-aud.mkdir(exist_ok=True)
-
-video_folder = folder_for_parse + '/video'
-vi = Path(video_folder)
-vi.mkdir(exist_ok=True)
-
-archives_folder = folder_for_parse + '/archives'
-arch = Path(archives_folder)
-arch.mkdir(exist_ok=True)
-
-
-
-
 def sorting_files(path: Path):
-
+    global im, vi, doc, aud, arch
     images = ['.png', '.jpeg', '.svg', '.jpg', '.JPEG', '.PNG', '.JPG', '.SVG']
     videos = ['.avi', '.mp4', '.mov', '.mkv', '.AVI', '.MP4', '.MOV', '.MKV']
     documents = ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx','.DOC', '.DOCX', '.TXT', '.PDF', '.XLSX', '.PPTX']
@@ -47,18 +19,27 @@ def sorting_files(path: Path):
             p = new_path.absolute()
             if p == im.absolute() or p == doc.absolute() or p == aud.absolute() or p == vi.absolute() or p == arch.absolute():
                 continue
+            elif len(os.listdir(p)) == 0:
+                new_path.rmdir()
+                print(f'Deleted emply folder: {new_path}')
+                continue
             else:
                 sorting_files(new_path)
         elif i.suffix in images:
             shutil.move(new_path, im)
+            print(f"Move image '{new_path}' to folder {im.name}")
         elif i.suffix in videos:
             shutil.move(new_path, vi)
+            print(f"Move video '{new_path}' to folder {vi.name}")
         elif i.suffix in documents:
             shutil.move(new_path, doc)
+            print(f"Move document '{new_path}' to folder {doc.name}")
         elif i.suffix in audios:
             shutil.move(new_path, aud)
+            print(f"Move audio '{new_path}' to folder {aud.name}")
         elif i.suffix in archives:
             shutil.move(new_path, arch)
+            print(f"Move archive '{new_path}' to folder {arch.name}")
  
 
 legend = {
@@ -139,12 +120,66 @@ def normalize(path: Path):
     new_filename = re.sub(regex, "_", new_filename)
 
     if new_filename != filename:
-        return path.rename(new_filename)
+        x = path.rename(new_filename)
+        print(f"Renamed '{filename}' to '{new_filename}'")
+        return x
+
     return path
         
 
+def unzip(folder:Path):
+    for i in folder.iterdir():
+        split_name = i.name.split('.')
+        name_folder = split_name[0]
+        a = str(folder) + "/" + name_folder
+        shutil.unpack_archive(i, a)
+        print(f"Unzipped {name_folder} to {a}")
 
-path = Path(folder_for_parse)
-sorting_files(path)
+
+im = None
+doc = None
+aud = None
+vi = None
+arch = None
+
+if __name__ == "__main__":
+    folder_for_parse = sys.argv[1]
+    path = Path(folder_for_parse)
+ 
+
+
+    images_folder = folder_for_parse + '/images'
+    im = Path(images_folder)
+    im.mkdir(exist_ok=True)
+    print('Created images folder')
+
+
+    documents_folder = folder_for_parse + '/documents'
+    doc = Path(documents_folder)
+    doc.mkdir(exist_ok=True)
+    print('Created documents folder')
+
+
+    audio_folder = folder_for_parse + '/audio'
+    aud = Path(audio_folder)
+    aud.mkdir(exist_ok=True)
+    print('Created audios folder')
+
+
+    video_folder = folder_for_parse + '/video'
+    vi = Path(video_folder)
+    vi.mkdir(exist_ok=True)
+    print('Created videos folder')
+
+    archives_folder = folder_for_parse + '/archives'
+    arch = Path(archives_folder)
+    arch.mkdir(exist_ok=True)
+    print('Created archives folder')
+
+    sorting_files(path)
+
+    unzip(arch)
+
+    print("Program is complited")
 
 
